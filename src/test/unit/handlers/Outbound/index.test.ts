9ba60ee2bf951f05f593a6b9db04b55bfa1bd2c2
Steven Oderayi <oderayi@gmail.com>
2021-06-18 18:50:57 +0100

    Add tests for OutboundHandler/index.ts

--- src/test/unit/handlers/Outbound/index.test.ts
+++ src/test/unit/handlers/Outbound/index.test.ts
@@ -8,14 +8,46 @@
  *      Steven Oderayi - steven.oderayi@modusbox.com                      *
  **************************************************************************/
 
- 'use strict'
+'use strict'
 
- describe('OutboundHandler', () => {
-     it('should return correct handler given a valid ISO 20022 message', async () => {
+import { Logger } from '@mojaloop/sdk-standard-components';
+import { mocked } from 'ts-jest/utils';
+import { OutboundHandler } from '../../../../handlers/Outbound';
 
-     });
+import camt003Handler from '../../../../handlers/Outbound/camt003Handler';
+jest.mock('../../../../handlers/Outbound/camt003Handler');
+const mockedHandler = mocked(camt003Handler, true);
 
-     it('should handle exception thrown by message-type handlers', async () => {
+describe('OutboundHandler', () => {
+    const logger = new Logger.Logger();
+    const ctx = {
+        request: {
+            body: {
+                Document: {
+                    $: {
+                        xmlns: 'urn:iso:std:iso:20022:tech:xsd:camt.003.001.07'
+                    }
+                }
+            }
+        },
+        state: {
+            logger
+        },
+        response: {}
+    }
 
-     });
- });
+    it('should return correct handler given a valid ISO 20022 message', async () => {
+        await OutboundHandler(ctx as any);
+        expect(mockedHandler).toBeCalledTimes(1);
+        expect(mockedHandler).toBeCalledWith(ctx);
+    });
+
+    it('should handle exception thrown by message-type handlers', async () => {
+        const loggerSpy = jest.spyOn(logger, 'error');
+        const error = new Error('Handler error');
+        mockedHandler.mockRejectedValueOnce(error);
+        await OutboundHandler(ctx as any);
+        expect(mockedHandler).toBeCalledWith(ctx);
+        expect(loggerSpy).toBeCalledWith(error);
+    });
+});
