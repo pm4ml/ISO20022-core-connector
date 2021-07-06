345ebe9c8ed22fcaad1b44d960b5d7d3a95be56e
Steven Oderayi <oderayi@gmail.com>
2021-07-06 08:37:41 +0100

    Update test

--- src/test/unit/lib/iso20022.test.ts
+++ src/test/unit/lib/iso20022.test.ts
@@ -8,18 +8,22 @@
  *      Steven Oderayi - steven.oderayi@modusbox.com                      *
  **************************************************************************/
 
+import * as uuid from 'uuid';
+jest.mock('uuid')
+
 import { generateMsgId } from '../../../lib/iso20022';
 
 describe('iso20022', () => {
     describe('generateMsgId', () => {
         it('should generate valid ISO20022 message Id', () => {
+            const uuidSpy = jest.spyOn(uuid, 'v4').mockReturnValue('a0b4bc34-6cb5-485b-8dff-d9e55cb11e7b')
             const mockDate = new Date('10/10/2010 10:10:10')
             jest
                 .spyOn(global, 'Date')
                 .mockImplementation(() => mockDate as unknown as string)
             const id = generateMsgId();
-            expect(id.slice(0,19)).toBe('RNDS/20101010101010');
-            expect(id.length).toEqual(27);
+            expect(id).toBe(`RNDS/20101010101010a0b4bc34`)
+            expect(uuidSpy).toHaveBeenCalledTimes(1);
         })
     })
 })
