2e9ca33e6df275e4f7ca1f618e0911698078d44e
Miguel de Barros <miguel@debarros.me>
2021-12-10 11:24:23 +0200

    updated header

--- src/test/helpers/mockRequesters.ts
+++ src/test/helpers/mockRequesters.ts
@@ -1,3 +1,13 @@
+/**************************************************************************
+ *  (C) Copyright ModusBox Inc. 2021 - All rights reserved.               *
+ *                                                                        *
+ *  This file is made available under the terms of the license agreement  *
+ *  specified in the corresponding source code repository.                *
+ *                                                                        *
+ *  ORIGINAL AUTHOR:                                                      *
+ *       miguel de Barros - miguel.de.barros@modusbox.com                 *
+ **************************************************************************/
+
 import { AxiosInstance, AxiosResponse } from "axios";
 import { MockedObjectDeep } from "ts-jest/dist/utils/testing";
 import { InboundRequester, OutboundRequester, RequesterOptions } from "../../requests";
